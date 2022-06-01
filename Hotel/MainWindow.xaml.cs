93ddd5b49b48a551c2907b28923efe0ba872c170
DarkB0B <miecze123@onet.pl>
2022-06-01 02:11:14 +0200

    "Pobyty" window is somewhat working TODO add "ADD" and "DELETE" buttons

--- Hotel/MainWindow.xaml.cs
+++ Hotel/MainWindow.xaml.cs
@@ -12,6 +12,11 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using System.Data.Entity;
+using System.Configuration;
+using System.Collections.Specialized;
+using System.Data;
+using System.Data.SqlClient;
+
 
 namespace Hotel
 {
@@ -27,31 +32,75 @@ namespace Hotel
         {
             InitializeComponent();
             pobytyViewSource = ((CollectionViewSource)(FindResource("pobytyViewSource")));
-            klienciViewSource = ((CollectionViewSource)(FindResource("klienciViewSource")));
+            //klienciViewSource = ((CollectionViewSource)(FindResource("klienciViewSource")));
             DataContext = this;
 
         }
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            
-            context.Pobyty.Load();
-           pobytyViewSource.Source = context.Pobyty.Local;
-            
-            // Load data by setting the CollectionViewSource.Source property:
-             klienciViewSource.Source = context.Klienci.Local;
-        }
-        private void PreviousCommandHandler(object sender, ExecutedRoutedEventArgs e)
-        {
-            
-            klienciViewSource.View.MoveCurrentToPrevious();
-        }
 
-        private void NextCommandHandler(object sender, ExecutedRoutedEventArgs e)
+            context.Pobyty.Load();
+            // pobytyViewSource.Source = context.Pobyty.Local;
+           
+            // Load data by setting the CollectionViewSource.Source property:
+            //klienciViewSource.Source = context.Klienci.Local;
+            System.Windows.Data.CollectionViewSource pobytyViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("pobytyViewSource")));
+            // Load data by setting the CollectionViewSource.Source property:
+            // pobytyViewSource.Source = [generic data source]
+        }
+        
+        
+        private void Rezerwacje_Click(object sender, RoutedEventArgs e)
         {
-            context.Klienci.Load();
-            klienciViewSource.Source = context.Klienci.Local;
-            klienciViewSource.View.MoveCurrentToNext();
+            string ConString = ConfigurationManager.ConnectionStrings["ConString"].ConnectionString;
+            string CmdString = string.Empty;
+            using (SqlConnection con = new SqlConnection(ConString))
+            {
+                CmdString = "select k.Imie as 'Imie Klienta', k.Nazwisko as 'Nazwisko Klienta', k.IdKlienta as 'Id Klienta',p.IdPokoju as 'Nr Pokoju',m.DataWyjazdu as 'Data Wyjazdu', u.Usluga 'Wykupiona Usuga', CenaPokoju*(DATEDIFF(day,DataPrzyjazdu,DataWyjazdu))+u.CenaUslugi as 'Cena Pobytu' from Pobyty m  inner join pokoje p on m.IdPokoju = P.IdPokoju inner join CenaPokoju c on p.IdCenyPokoju = c.IdCenyPokoju inner join Klienci k on m.IdKlienta = k.IdKlienta inner join Uslugi u on m.IdUslugi = u.IdUslugi where  DataWyjazdu > GETDATE() and DataPrzyjazdu > GETDATE()";
+                SqlCommand cmd = new SqlCommand(CmdString, con);
+                SqlDataAdapter sda = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable("Pokoje");
+                sda.Fill(dt);
+                grdPobyty.ItemsSource = dt.DefaultView;
+            }
+            Pobyty.Content = "Rezerwacje";
+        }
+        
+        private void HistoriaPobytow_Click(object sender, RoutedEventArgs e)
+        {
+            string ConString = ConfigurationManager.ConnectionStrings["ConString"].ConnectionString;
+            string CmdString = string.Empty;
+            using (SqlConnection con = new SqlConnection(ConString))
+            {
+                CmdString = "select k.Imie as 'Imie Klienta', k.Nazwisko as 'Nazwisko Klienta', k.IdKlienta as 'Id Klienta',p.IdPokoju as 'Nr Pokoju',m.DataWyjazdu as 'Data Wyjazdu', u.Usluga 'Wykupiona Usuga', CenaPokoju*(DATEDIFF(day,DataPrzyjazdu,DataWyjazdu))+u.CenaUslugi as 'Cena Pobytu' from Pobyty m  inner join pokoje p on m.IdPokoju = P.IdPokoju inner join CenaPokoju c on p.IdCenyPokoju = c.IdCenyPokoju inner join Klienci k on m.IdKlienta = k.IdKlienta inner join Uslugi u on m.IdUslugi = u.IdUslugi where DataPrzyjazdu < GETDATE()";
+                SqlCommand cmd = new SqlCommand(CmdString, con);
+                SqlDataAdapter sda = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable("Pokoje");
+                sda.Fill(dt);
+                grdPobyty.ItemsSource = dt.DefaultView;
+            }
+            Pobyty.Content = "HistoriaPobytow";
+        }
+        
+        private void AktualnePobyty_Click(object sender, RoutedEventArgs e)
+        {
+            string ConString = ConfigurationManager.ConnectionStrings["ConString"].ConnectionString;
+            string CmdString = string.Empty;
+            using (SqlConnection con = new SqlConnection(ConString))
+            {
+                CmdString = "select k.Imie as 'Imie Klienta', k.Nazwisko as 'Nazwisko Klienta', k.IdKlienta as 'Id Klienta',p.IdPokoju as 'Nr Pokoju',m.DataWyjazdu as 'Data Wyjazdu', u.Usluga 'Wykupiona Usuga', CenaPokoju*(DATEDIFF(day,DataPrzyjazdu,DataWyjazdu))+u.CenaUslugi as 'Cena Pobytu' from Pobyty m  inner join pokoje p on m.IdPokoju = P.IdPokoju inner join CenaPokoju c on p.IdCenyPokoju = c.IdCenyPokoju inner join Klienci k on m.IdKlienta = k.IdKlienta inner join Uslugi u on m.IdUslugi = u.IdUslugi where  DataWyjazdu > GETDATE() and DataPrzyjazdu < GETDATE()";
+                SqlCommand cmd = new SqlCommand(CmdString, con);
+                SqlDataAdapter sda = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable("Pokoje");
+                sda.Fill(dt);
+                grdPobyty.ItemsSource = dt.DefaultView;
+            }
+            Pobyty.Content = "AktualnePobyty";
+        }
+        
+        private void grdPobyty_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
             
         }
     }
